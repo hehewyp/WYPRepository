904032a64da3bf2c547672fdd155e31ef623b139
hehewyp <hehewyp@192.168.0.115>
2015-01-19 20:43:03 +0800

    init

---
+++ WYPHelloWorld/WYPHelloWorld/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WYPHelloWorld
+//
+//  Created by hehewyp on 15/1/19.
+//  Copyright (c) 2015年 hehewyp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
